c68c54fc5e0863665f50566218381322296da49d
putheakhem <puthea.khem@gmail.com>
2021-04-02 14:14:03 +0700

    fixed typo

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@
 
 CUDA_VISIBLE_DEVICES=0 fairseq-train \
     data/all-clean-km/data_bin/ \
-    --source-lang ne --target-lang en \
+    --source-lang en --target-lang km \
     --arch transformer --share-all-embeddings \
     --encoder-layers 5 --decoder-layers 5 \
     --encoder-embed-dim 512 --decoder-embed-dim 512 \
@@ -21,7 +21,7 @@ CUDA_VISIBLE_DEVICES=0 fairseq-train \
     --label-smoothing 0.2 --criterion label_smoothed_cross_entropy \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0 \
     --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-7 \
-    --lr 1e-3 --min-lr 1e-9 \
+    --lr 1e-3 \
     --max-tokens 4000 \
     --update-freq 4 \
     --max-epoch 100 --save-interval 10
