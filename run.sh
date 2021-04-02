b3a6ee2be097920acbd26b6330310f63fac15a4b
putheakhem <puthea.khem@gmail.com>
2021-04-02 13:25:29 +0700

    add run.sh for trainning

---
+++ run.sh
@@ -0,0 +1,27 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#!/bin/bash
+
+
+CUDA_VISIBLE_DEVICES=0 fairseq-train \
+    data/all-clean-km/data_bin/ \
+    --source-lang ne --target-lang en \
+    --arch transformer --share-all-embeddings \
+    --encoder-layers 5 --decoder-layers 5 \
+    --encoder-embed-dim 512 --decoder-embed-dim 512 \
+    --encoder-ffn-embed-dim 2048 --decoder-ffn-embed-dim 2048 \
+    --encoder-attention-heads 2 --decoder-attention-heads 2 \
+    --encoder-normalize-before --decoder-normalize-before \
+    --dropout 0.4 --attention-dropout 0.2 --relu-dropout 0.2 \
+    --weight-decay 0.0001 \
+    --label-smoothing 0.2 --criterion label_smoothed_cross_entropy \
+    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0 \
+    --lr-scheduler inverse_sqrt --warmup-updates 4000 --warmup-init-lr 1e-7 \
+    --lr 1e-3 --min-lr 1e-9 \
+    --max-tokens 4000 \
+    --update-freq 4 \
+    --max-epoch 100 --save-interval 10
